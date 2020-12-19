0467d35c7a791053f93599b38217b3960d6e2bd2
0x7943 <0x7943@protonmail.com>
2020-12-19 05:41:43 -0800

    dev

---
+++ PinkPill.ConsoleTest/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace PinkPill.ConsoleTest
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            // this project is for internal use
+        }
+    }
+}
